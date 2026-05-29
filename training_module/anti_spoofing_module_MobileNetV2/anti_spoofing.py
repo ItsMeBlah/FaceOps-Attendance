9b897537e11be38fcfd7d3adeda2cab76bdea241
Duc Huy BUI <104788737@student.swin.edu.au>
2026-05-29 15:06:17 +1000

    Enable Memory Growth for antispoofing

--- training_module/anti_spoofing_module_MobileNetV2/anti_spoofing.py
+++ training_module/anti_spoofing_module_MobileNetV2/anti_spoofing.py
@@ -14,6 +14,14 @@ import tensorflow as tf
 import tf2onnx
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
 
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
+
 
 #General configuration
 
